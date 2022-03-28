50665466cd21ae2b41c666e1566efc83713642f4
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-03-28 15:46:34 +0530

    React Handling side effects [Post Class] - Timed Golf

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,45 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
+
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" }
+    };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
   }
+
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  renderChoice() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else
+      return (
+        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
+      );
+  }
+
+  onKeyPressed(e) {
+    if (e.keyCode === 39 && this.state.renderBall === true) {
+      this.setState({ posi: this.state.posi + 5 }, () =>
+        this.setState({ ballPosition: { left: this.state.posi + "px" } })
+      );
+    } else return;
+  }
+
   componentDidMount() {
-    
+    document.addEventListener("keydown", this.onKeyPressed.bind(this));
   }
-
-  componentWillUnmount() {
-    
-  }
-
-
 
   render() {
-    return (
- <>
-</>
-    );
+    return <div className="playground">{this.renderChoice()}</div>;
   }
 }
 
-export default Timer;
+export default App;
